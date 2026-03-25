9eb8800ada410932667d1bb91a6d600071d41bd3
alfonso7199 <alfonso1799@gmail.com>
2026-03-25 19:29:39 +0100

    Canales creados v1

---
+++ includes/Channel.hpp
@@ -0,0 +1,57 @@
+#ifndef CHANNEL_HPP
+# define CHANNEL_HPP
+
+# include <string>
+# include <map>
+# include <set>
+# include "Client.hpp"
+
+class Channel
+{
+	private:
+		std::string				_name;
+		std::string				_topic;
+		std::string				_passwordChannel;
+		int						_userlimit;
+		bool					_inviteOnly;
+		bool					_topicRestricted;
+		std::map<int, Client *>	_members;
+		std::set<int>			_admins;
+		std::set<int>			_invited;
+
+	public:
+		Channel(const std::string &name);
+		Channel(const Channel &other);
+		Channel	&operator=(const Channel &other);
+		~Channel(void);
+
+		const std::string			&getName(void) const;
+		const std::string			&getTopic(void) const;
+		const std::string			&getPasswordChannel(void) const;
+		int							getUserLimit(void) const;
+		bool						isInviteOnly(void) const;
+		bool						isTopicRestricted(void) const;
+		int							getMemberCount(void) const;
+		std::map<int, Client *>		&getMembers(void);
+
+		void	setTopic(const std::string &topic);
+		void	setPasswordChannel(const std::string &key);
+		void	setLimit(int limit);
+		void	setInviteOnly(bool val);
+		void	setTopicRestricted(bool val);
+
+		void	addMember(Client *client);
+		void	removeMember(int fd);
+		bool	hasMember(int fd) const;
+
+		void	addAdmin(int fd);
+		void	removeAdmin(int fd);
+		bool	isAdmin(int fd) const;
+
+		void	addInvited(int fd);
+		bool	isInvited(int fd) const;
+
+		void	broadcast(const std::string &msg, int excludeFd = -1) const;
+};
+
+#endif
---
+++ sources/Channel.cpp
@@ -0,0 +1,156 @@
+#include "Channel.hpp"
+
+Channel::Channel(const std::string &name) :
+_name(name), _topic(""), _passwordChannel(""), _userlimit(0),
+_inviteOnly(false), _topicRestricted(false)
+{
+	return ;
+}
+
+Channel::Channel(const Channel &other) :
+_name(other._name), _topic(other._topic), _passwordChannel(other._passwordChannel),
+_userlimit(other._userlimit), _inviteOnly(other._inviteOnly),
+_topicRestricted(other._topicRestricted), _members(other._members),
+_admins(other._admins), _invited(other._invited)
+{
+	return ;
+}
+
+Channel	&Channel::operator=(const Channel &other)
+{
+	if (this != &other)
+	{
+		this->_name = other._name;
+		this->_topic = other._topic;
+		this->_passwordChannel = other._passwordChannel;
+		this->_userlimit = other._userlimit;
+		this->_inviteOnly = other._inviteOnly;
+		this->_topicRestricted = other._topicRestricted;
+		this->_members = other._members;
+		this->_admins = other._admins;
+		this->_invited = other._invited;
+	}
+	return (*this);
+}
+
+Channel::~Channel(void)
+{
+	return ;
+}
+
+const std::string	&Channel::getName(void) const
+{
+	return (this->_name);
+}
+
+const std::string	&Channel::getTopic(void) const
+{
+	return (this->_topic);
+}
+
+const std::string	&Channel::getPasswordChannel(void) const
+{
+	return (this->_passwordChannel);
+}
+
+int	Channel::getUserLimit(void) const
+{
+	return (this->_userlimit);
+}
+
+bool	Channel::isInviteOnly(void) const
+{
+	return (this->_inviteOnly);
+}
+
+bool	Channel::isTopicRestricted(void) const
+{
+	return (this->_topicRestricted);
+}
+
+int	Channel::getMemberCount(void) const
+{
+	return (this->_members.size());
+}
+
+std::map<int, Client *>	&Channel::getMembers(void)
+{
+	return (this->_members);
+}
+
+void	Channel::setTopic(const std::string &topic)
+{
+	this->_topic = topic;
+}
+
+void	Channel::setPasswordChannel(const std::string &key)
+{
+	this->_passwordChannel = key;
+}
+
+void	Channel::setLimit(int limit)
+{
+	this->_userlimit = limit;
+}
+
+void	Channel::setInviteOnly(bool val)
+{
+	this->_inviteOnly = val;
+}
+
+void	Channel::setTopicRestricted(bool val)
+{
+	this->_topicRestricted = val;
+}
+
+void	Channel::addMember(Client *client)
+{
+	this->_members[client->getFd()] = client;
+}
+
+void	Channel::removeMember(int fd)
+{
+	this->_members.erase(fd);
+	this->_admins.erase(fd);
+	this->_invited.erase(fd);
+}
+
+bool	Channel::hasMember(int fd) const
+{
+	return (this->_members.count(fd) > 0);
+}
+
+void	Channel::addAdmin(int fd)
+{
+	this->_admins.insert(fd);
+}
+
+void	Channel::removeAdmin(int fd)
+{
+	this->_admins.erase(fd);
+}
+
+bool	Channel::isAdmin(int fd) const
+{
+	return (this->_admins.count(fd) > 0);
+}
+
+void	Channel::addInvited(int fd)
+{
+	this->_invited.insert(fd);
+}
+
+bool	Channel::isInvited(int fd) const
+{
+	return (this->_invited.count(fd) > 0);
+}
+
+void	Channel::broadcast(const std::string &msg, int excludeFd) const
+{
+	std::map<int, Client *>::const_iterator it;
+	for (it = _members.begin(); it != _members.end(); ++it)
+	{
+		if (it->first != excludeFd)
+			it->second->send(msg);
+	}
+}
