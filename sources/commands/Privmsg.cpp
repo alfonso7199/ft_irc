c412167ce48e2eb8db1a3d1143326e3df5a90f83
alfon7199 <alfonso1799@gmail.com>
2026-04-06 18:33:49 +0200

    move privmsg to commands/privmsg

--- sources/commands/Privmsg.cpp
+++ sources/commands/Privmsg.cpp
@@ -2,6 +2,64 @@
 
 void	Server::cmdPrivmsg(int fd, const std::string &params)
 {
-	(void)fd;
-	(void)params;
+	Client		&client = this->_clients.find(fd)->second;
+	size_t		space = params.find(' ');
+
+	if (!client.isRegistered())
+	{
+		sendReply(fd, ":" + this->_name + ERR_NOTREGISTERED + "* :You have not registered");
+		return ;
+	}
+
+	if (params.empty() || space == std::string::npos)
+	{
+		if (params.empty())
+			sendReply(fd, ":" + this->_name + ERR_NORECIPIENT + client.getNickname() + " :No recipient givem (PRIVMSG)");
+		else
+			sendReply(fd, ":" + this->_name + ERR_NOTEXTTOSEND + client.getNickname() + " :No text to send");
+		return ;
+	}
+
+	std::string	message = params.substr(space + 1);
+	if (!message.empty() && message[0] == ':')
+		message = message.substr(1);
+	if (message.empty())
+	{
+		sendReply(fd, ":" + this->_name + ERR_NOTEXTTOSEND + client.getNickname() + " :No text to send");
+		return ;
+	}
+
+	std::string	target = params.substr(0, space);
+	std::string	prefix = ":" + client.getNickname() + "!" + client.getUsername() + "@" + client.getHostname();
+
+	if (target[0] == '#')
+	{
+		if (!this->_channels.count(target))
+		{
+			sendReply(fd, ":" + this->_name + ERR_NOSUCHCHANNEL + client.getNickname() + " " + target + " :No such channel");
+			return ;
+		}
+
+		Channel	&channel = this->_channels.find(target)->second;
+		if (!channel.hasMember(fd))
+		{
+			sendReply(fd, ":" + this->_name + ERR_CANNOTSENDTOCHAN + client.getNickname() + " " + target + " :Cannot send to channel");
+			return;
+		}
+		channel.broadcast(prefix + " PRIVMSG " + target + " :" + message, fd);
+	}
+	else
+	{
+		std::map<int, Client>::iterator	it = this->_clients.begin();
+		while (it != this->_clients.end())
+		{
+			if (it->second.getNickname() == target)
+			{
+				sendReply(it->first, prefix + " PRIVMSG " + target + " :" + message);
+				return ;
+			}
+			++it;
+		}
+		sendReply(fd, ":" + this->_name + ERR_NOSUCHNICK + client.getNickname() + " " + target + " :No such nick/channel");
+	}
 }
